62322f2706ecf122c39784b7657664ce4baf9037
JoaoGuedesLuna <joaoguedesluna@gmail.com>
2024-05-06 01:14:45 -0300

    :recycle: Refactoring .properties file loading from a static method to a static block

--- src/main/java/dev/guedes/jdbcdemo/database/Database.java
+++ src/main/java/dev/guedes/jdbcdemo/database/Database.java
@@ -2,6 +2,8 @@ package dev.guedes.jdbcdemo.database;
 
 import dev.guedes.jdbcdemo.exception.DatabaseException;
 import dev.guedes.jdbcdemo.exception.PropertiesException;
+import lombok.Cleanup;
+import lombok.experimental.UtilityClass;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.Connection;
@@ -12,33 +14,30 @@ import java.util.Properties;
 /**
  * @author João Guedes
  */
+@UtilityClass
 public class Database {
+
+    private static final Properties properties;
+
+    static {
+        String propertiesPath = System.getProperty("user.dir").concat("\\src\\main\\resources\\application.properties");
+        try {
+            @Cleanup FileInputStream fileInputStream = new FileInputStream(propertiesPath);
+            properties = new Properties();
+            properties.load(fileInputStream);
+        } catch (IOException e) {
+            throw new PropertiesException(e.getMessage());
+        }
+    }
 
     public static Connection getConnection() throws DatabaseException {
         try {
-            Properties properties = Database.loadProperties();
             final String URL = properties.getProperty("jdbc.datasource.url");
             final String USERNAME = properties.getProperty("jdbc.datasource.username");
             final String PASSWORD = properties.getProperty("jdbc.datasource.password");
             return DriverManager.getConnection(URL, USERNAME, PASSWORD);
-        }
-        catch (PropertiesException e) {
-            throw new RuntimeException(e.getMessage());
-        }
-        catch (SQLException e) {
+        } catch (SQLException e) {
             throw new DatabaseException(e.getMessage());
-        }
-    }
-
-    private static Properties loadProperties() throws PropertiesException {
-        String propertiesPath = System.getProperty("user.dir").concat("\\src\\main\\resources\\application.properties");
-        try {
-            FileInputStream fileInputStream = new FileInputStream(propertiesPath);
-            Properties properties = new Properties();
-            properties.load(fileInputStream);
-            return properties;
-        } catch (IOException e) {
-            throw new PropertiesException(e.getMessage());
         }
     }
 
